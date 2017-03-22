c95ba4f02e8df88ab4fbf6f9ec59ae054b8270e2
jhuber3 <jhuber3@github.com>
2017-03-21 23:58:34 -0400

    created build system

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+// DUMMY DRIVER TO TEST CLASSES
+
+#include <iostream>
+#include "Node.hpp"
+#include "Graph.hpp"
+
+int main() {
+/*
+   Graph myMap;
+
+   myMap.addCity("city1",234.234,3.524);
+   myMap.addCity("city2",5786.36,746.746);
+   myMap.addCity("city3",346.2,986.5876);
+*/
+
+   Graph myMap("cities.json");
+
+   myMap.printCities();
+}
