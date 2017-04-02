65feae5d67bd1e30a1c4d05eb76b1c02140ea90a
jhuber3 <jhuber3@github.com>
2017-04-02 14:09:38 -0400

    added folder and file for testing

---
+++ test/TravelingSalesman_test.cpp
@@ -0,0 +1,28 @@
+#define BOOST_TEST_DYN_LINK 1
+#include <boost/test/unit_test.hpp>
+
+#include "../include/Graph.hpp"
+
+/*
+Function to check whether the predicted distance is within a 10km
+bound of the true distance. We permit the predicted distance to be within
+a certain range to allow for numerical inaccuracies that may arise in the 
+computation.
+*/
+bool withinRange(double dist_pred, double dist_true)
+{
+	double lower_bound = dist_true - 5.0;
+	double upper_bound = dist_true + 5.0;
+
+	return !(dist_pred < lower_bound) && !(upper_bound < dist_pred); 
+}
+
+// check that the calcDist function in the Graph class is working 
+BOOST_AUTO_TEST_CASE(distance_calculator)
+{
+	Graph myMap("../../data/cities.json");
+
+	BOOST_CHECK(withinRange(myMap.calcDist("New York", "Boston"), 301.6));
+	BOOST_CHECK(withinRange(myMap.calcDist("Portland", "Tucson"), 1785.6));
+	BOOST_CHECK(withinRange(myMap.calcDist("Tulsa", "Tampa"), 1562.9));
+}
