c251158eec9853142bcce7a726c202563fe05c65
alubera <alubera@nd.edu>
2017-04-06 00:40:11 -0400

    a boost graph finally compiles, but the Graph class is still a mess...major compile errors when trying to add an edge

--- include/Graph.hpp
+++ include/Graph.hpp
@@ -3,118 +3,61 @@
  * Graph.hpp
  * class will act as data structure to hold all points that travelling salesman will hit
  * all cities are held in an unordered_set to try for most efficient lookups
+ * READER NEEDS TO BE CALLED FIRST SO THAT GRAPH SIZE IS KNOWN
  */
 
 #ifndef GRAPH_HPP
 #define GRAPH_HPP
 
+#include "Node.hpp"
 #include <boost/config.hpp>
 #include <boost/graph/adjacency_matrix.hpp>
 #include <cctype>
 #include <cassert>
 #include <cmath>
 #include <exception>
-#include <fstream>
-#include <sstream>
 #include <string>
-#include <unordered_map>
 #include <tuple>
 #include <iostream>
+#include <vector>
 
 using namespace boost;
 
 typedef property<edge_weight_t, double> Weight;
 typedef adjacency_matrix<undirectedS,Weight> UGraph;
-typedef graph_traits<UGraph>::edge_iterator EdgeIterator;
+//typedef graph_traits<UGraph>::edge_iterator EdgeIterator;
 
 class Graph {
- 
+
    public:
-      Graph();
-      Graph(const std::string &);
-      void addCity(std::string,double,double);
-      std::tuple<double,double> getCity(std::string);
+      Graph(const std::vector<Node> &);
+/*      std::tuple<double,double> getCity(std::string);
       void const printCities();
       double const calcDist(std::string,std::string);
-
+*/
    private:
-      // using strings as keys for easy lookup for distance calculations
-      // tuple will hold long and lat coordinates
-      //std::unordered_map<std::string, std::tuple<double,double> > cities;
+      // boost graph (see typedef for adjacency matrix) will hold all distances
       UGraph ug;
+      // implement a data structure so city names and ID's are known?
+      const std::vector<Node> cityNames;
 };
 
-Graph::Graph() : ug(size) {};
-
 // non-default constructor will take a file name and read it
-Graph::Graph(const std::string &filename) : ug(size) {
+Graph::Graph(const std::vector<Node> &cities) 
+         : ug(cities.size()),
+            cityNames(cities) {
+/*
+   typename graph_traits<UGraph>::vertex_descriptor u,v;
+   typedef typename UGraph::edge_property_type Weight;
+   typename property_map<UGraph,edge_weight_t>::type weight = get(edge_weight,ug);
+   typename graph_traits<UGraph>::edge_descriptor e1, e2;
+   bool found;
 
-   //typename graph_traits<UGraph>::vertex_descriptor u,v;
-   //u = vertex(0,ug);
-   //v = vertex(1,ug);
+   u = vertex(0,ug);
+   v = vertex(1,ug);*/
    //add_edge(u,v,Weight(10),ug);
-   add_edge(0,1,Weight(10),ug);
-
-   std::ifstream fp(filename);
-
-   if (!fp) {
-     std::cout << "file not opened" << std::endl;
-   }
-
-   std::string line, key, value;
-   std::string city;
-   double lon, lat;
-   int i = 0;
-
-   // this parsing is kinda janky...but it gets the job done
-   while (getline(fp,line)) {
-
-      // do all of this for each line in the JSON file
-      key.clear();
-      value.clear();
-      auto s_itr = line.begin();
-      // increment string pointer until it hits first character
-      while (!isalpha(*s_itr) && s_itr != line.end()) {
-         ++s_itr;
-      }
-      // if line is garbage then move on to the next one
-      if (s_itr == line.end())
-         continue;
-      // add alpha characters to key (see what json key for line is)
-      while (isalpha(*s_itr))
-         key += *(s_itr++);
-      // move on to next line if it is not one we want
-      if (key != "city" && key != "latitude" && key != "longitude")
-         continue;
-      // increment string pointer until it hits next alpha character
-      while (!isalnum(*s_itr) && *s_itr != '-')
-         ++s_itr;
-      // add alpha characters to key (see what json key for line is)
-      while (isalnum(*s_itr) || isspace(*s_itr) || *s_itr == '-' || *s_itr == '.' || *s_itr == '\'')
-         value += *(s_itr++);
-
-      // insert found info into node in cities set
-      if (key == "city") {
-         city = value;
-      } else if (key == "latitude") {
-         lat = std::stod(value);
-      } else if (key == "longitude") {
-         lon = std::stod(value);
-      }
-
-      ++i;
-      if (i%3 == 0) {
-         addCity(city,lat,lon);
-         i = 0;
-      }      
-   }
 }
-
-void Graph::addCity(std::string city, double lat, double lon) {
-   // just insert new city into graph with given params
-   cities[city] = std::make_tuple(lat,lon);
-}
-
+/*
 std::tuple<double,double> Graph::getCity(std::string name) {
    // returns tuple (lat,lon)
    // throw assert if city is not in graph
@@ -155,5 +98,5 @@ double const Graph::calcDist(std::string city1, std::string city2) {
    return 2.0 * EARTH_RADIUS * asin(sqrt(hav_dist));
 
 }
-
+*/
 #endif
