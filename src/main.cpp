ef48a3f31cec8954198b259eeabcf4957775da26
alubera <alubera@nd.edu>
2017-04-06 18:40:22 -0400

    driver updated as well

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 // DUMMY DRIVER TO TEST CLASSES
 
 #include <iostream>
-#include "Graph.hpp"
+#include "../include/Graph.hpp"
+#include "../include/Reader.hpp"
 
 int main() {
 
+   /*****************************************
+   *  MOST OF THIS IS NOW DEPRECATED FOR NOW
+   *////////////////////////////////////////
+  
    // try out manually adding cities...this should maybe be a private function
 /*   Graph myMap;
 
@@ -14,7 +19,7 @@ int main() {
 */
 
    // non-default constructor reads in file
-   Graph myMap("../../data/cities.json");
+//   Graph myMap("../../data/cities.json");
 
    // try out city get function
    //std::cout << std::get<0>(myMap.getCity("Detroit")) << std::endl;
@@ -22,5 +27,15 @@ int main() {
    //std::cout << std::get<0>(myMap.getCity("oit")) << std::endl;
 
    // try out printing function (could be cleaner spacing)
-   myMap.printCities();
+//   myMap.printCities();
+
+   /*************************************
+   *  END DEPRECATED
+   *///////////////////////////////////// 
+
+
+   Reader myReader("../data/cities.json");
+   auto res = myReader.getCities();
+
+   Graph myGraph(res);
 }
