7bf77ed6e63ee904a91cbedc0e359fa9336580fb
alubera <alubera@nd.edu>
2017-05-09 00:08:48 -0400

    og version of two approx heuristic...edge vector probably isnt appropriate for our uses so I am changing that now

--- include/Traversals.hpp
+++ include/Traversals.hpp
@@ -11,6 +11,9 @@
 #include <algorithm>
 #include <iterator>
 #include <iostream>
+#include <list>
+#include <unordered_set>
+#include <set>
 
 #include "Graph.hpp"
 #include "bfs_time_visitor.hpp"
@@ -41,8 +44,6 @@ void bfs_example(Graph myGraph, std::ofstream& myFile) {
    // get city lats and lons from Graph
    const auto& lats = myGraph.getCityLats();
    const auto& lons = myGraph.getCityLons();
-
-
 
    // print city names in order
    std::cout << "order of discovery:\n";
@@ -84,6 +85,8 @@ void mst_example(Graph myGraph) {
    }*/
 }
 
+// function computes minimum spanning tree of ug
+// then builds a new graph out of it (could be templated)
 void compute_mst(UGraph& ug, MultiGraph& mst) {
 
    int graphSize = num_vertices(ug);
@@ -101,6 +104,97 @@ void compute_mst(UGraph& ug, MultiGraph& mst) {
    }
 }
 
+// function duplicates all edges in g
+// g can be a generic boost graph type
+template <typename GraphT>
+void dup_edges(GraphT& g) {
+
+   // local typedefs (since function is templated)
+   typedef typename graph_traits<GraphT>::edge_iterator EdgeIteratorT;
+   typedef typename graph_traits<GraphT>::edge_descriptor EdgeDescriptorT;
+
+   EdgeIteratorT ei, ei_end;
+
+   // first get all edges to duplicate in a list
+   std::list<EdgeDescriptorT> all_edges;
+   // if edges are added here then loop will be infinite
+   for (std::tie(ei,ei_end) = edges(g); ei != ei_end; ++ei) {
+      all_edges.push_back(*ei);
+   }
+
+   // then loop through list to dup edges in graph
+   for (auto it = all_edges.begin(); it != all_edges.end(); ++it) {
+      add_edge(target(*it,g),source(*it,g),g);
+   }
+}
+
+template <typename GraphT, typename EdgeT = typename graph_traits<GraphT>::edge_descriptor, typename VertexT = typename graph_traits<GraphT>::vertex_descriptor>
+EdgeT check_edge(const VertexT& u, const VertexT& v, const GraphT& g) {
+
+   EdgeT e;
+   bool exists;
+   tie(e,exists) = edge(u,v,g);
+   if (!exists) throw "Error: tried to use an edge that doesn't exist";
+   return e;
+}
+
+// function will compute a Eulerian Tour in graph g
+// a Eulerian Tour is a path that visits every edge exactly once
+// it is assumed that every edge will have an even degree, so the tour will be a circuit
+template <typename GraphT, typename EdgeT = typename graph_traits<GraphT>::edge_descriptor>
+void compute_euler(const GraphT& g, std::vector<EdgeT>& path) {
+
+   typedef typename graph_traits<GraphT>::vertex_descriptor VertexT;
+
+   // TODO: try every vertex as start
+   VertexT start_vertex = vertex(0,g);
+   
+   std::set<EdgeT> visited_edges;
+   std::set<VertexT> used_vs;
+   std::vector<VertexT> tmp;
+   std::vector<VertexT> order;
+
+   tmp.push_back(start_vertex);
+   while (!tmp.empty()) {
+      VertexT v = tmp.back();
+      typename graph_traits<GraphT>::out_edge_iterator ei, ei_end;
+      bool has_avail_edge = false;
+      // loop through all edges, checking for unmarked
+      for (tie(ei,ei_end) = out_edges(v,g); ei != ei_end; ++ei) {
+         // if current edge is unmarked
+         if (visited_edges.find(*ei) == visited_edges.end()) {
+            has_avail_edge = true;
+            //EdgeT e = *ei;
+            visited_edges.insert(*ei);
+            //EdgeT opposite_edge = edge(target(e,g),source(e,g),g).first;
+            //visited_edges.insert(opposite_edge);
+            if (v == target(*ei,g)) {
+               tmp.push_back(source(*ei,g));
+            } else {
+               tmp.push_back(target(*ei,g));
+            }
+            break;
+         }
+      }
+      if (!has_avail_edge) {
+         // skip vertices that have already been used in path
+         // this is called shortcutting
+         if (used_vs.find(v) == used_vs.end()) {
+            // add edge when there are two vertices to connect
+            if (!order.empty()) {
+               path.push_back(check_edge(order.back(),v,g));
+               order.pop_back();
+            }
+            std::cout << v << std::endl;
+            used_vs.insert(v);
+            order.push_back(v);
+         }
+         tmp.pop_back();   // move on to next vertex that was visited
+      }
+   }
+   // add last edge (should go back to first vertex)
+   //path.push_back(check_edge(order.back(),start_vertex,g));
+}
 
 
 void edge_iterators(Graph myGraph) {
--- include/TwoApprox.hpp
+++ include/TwoApprox.hpp
@@ -12,6 +12,7 @@
 void two_approx(Graph myGraph, std::vector<Vertex>& path) {
 
    UGraph& ug = myGraph.getGraphRef();
+   auto cities = myGraph.getCityNames();
    int graphSize = num_vertices(ug);
 
    // compute minimum spanning tree into new boost graph sturcture
@@ -19,8 +20,36 @@ void two_approx(Graph myGraph, std::vector<Vertex>& path) {
    Traversals::compute_mst(ug,mst);
 
    // duplicate all edges so that an euler cycle can be constructed easily
-   //Traversals::dup_edges(mst);
-}
+   Traversals::dup_edges<MultiGraph>(mst);
 
+   std::cout << num_edges(mst) << std::endl;
+
+   // compute Eulerian Tour of double mst
+   typedef typename graph_traits<MultiGraph>::edge_descriptor EdgeT;
+   std::vector<EdgeT> edge_path;
+   Traversals::compute_euler<MultiGraph>(mst,edge_path);
+
+   for (auto it = edge_path.begin(); it != edge_path.end(); ++it) {
+      std::cout << cities.at(source(*it,mst)) << " ==> " << cities.at(target(*it,mst)) << std::endl;
+   }
+
+   // playing around
+/*
+   typedef typename graph_traits<MultiGraph>::edge_descriptor EdgeT;
+   std::set<EdgeT> visited_edges;
+
+   typename graph_traits<MultiGraph>::out_edge_iterator ei, ei_end;
+   while (visited_edges.size() < 2) {
+      for (tie(ei,ei_end) = out_edges(0,mst); ei != ei_end; ++ei) {
+         if (visited_edges.find(*ei) == visited_edges.end()) {
+            visited_edges.insert(*ei);
+            std::cout << visited_edges.size() << std::endl;
+            break;
+         } else {
+            std::cout << "edge already visited" << std::endl;
+         }
+      }
+   }*/
+}
 
 #endif
